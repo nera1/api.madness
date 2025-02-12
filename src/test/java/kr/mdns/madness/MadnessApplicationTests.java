d1765c5df042afb7ca3ab263b6d4acd0432fccee
nera1 <rustydoor4728@gmail.com>
2025-02-12 15:17:24 +0900

    init spring boot project

---
+++ src/test/java/kr/mdns/madness/MadnessApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mdns.madness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
