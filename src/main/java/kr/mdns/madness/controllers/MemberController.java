9e6ae73959249f305cc53837337eb462400c0e86
nera1 <nera4936@gmail.com>
2025-05-16 19:09:01 +0900

    feat: add duplicate check endpoints for email and nickname

--- src/main/java/kr/mdns/madness/controllers/MemberController.java
+++ src/main/java/kr/mdns/madness/controllers/MemberController.java
@@ -16,26 +16,39 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/member")
 @Validated
 @RequiredArgsConstructor
 public class MemberController {
-    private final MemberService memberService;
+        private final MemberService memberService;
 
-    @PostMapping
-    public ResponseEntity<ApiResponse<SignupResponseDto>> register(
-            @Valid @RequestBody SignupRequestDto req) {
-        Member saved = memberService.register(req);
-        SignupResponseDto payload = SignupResponseDto
-                .builder()
-                .email(saved.getEmail())
-                .nickname(saved.getNickname())
-                .build();
-        ApiResponse<SignupResponseDto> resp = new ApiResponse<SignupResponseDto>(0, "signup success", payload);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(resp);
-    }
+        @PostMapping
+        public ResponseEntity<ApiResponse<SignupResponseDto>> register(
+                        @Valid @RequestBody SignupRequestDto req) {
+                Member saved = memberService.register(req);
+                SignupResponseDto payload = SignupResponseDto
+                                .builder()
+                                .email(saved.getEmail())
+                                .nickname(saved.getNickname())
+                                .build();
+                ApiResponse<SignupResponseDto> resp = new ApiResponse<SignupResponseDto>(0, "signup success", payload);
+                return ResponseEntity
+                                .status(HttpStatus.CREATED)
+                                .body(resp);
+        }
+
+        @GetMapping("/check/nickname")
+        public String checkNickname(@RequestParam String param) {
+                return new String();
+        }
+
+        @GetMapping("/check/email")
+        public String checkEmail(@RequestParam String param) {
+                return new String();
+        }
+
 }
