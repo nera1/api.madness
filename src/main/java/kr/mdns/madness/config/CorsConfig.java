be543005e85c7cd503cfa1dbb56be014dbcc17c5
nera1 <nera4936@gmail.com>
2025-05-21 01:39:18 +0900

    feat: remove CorsFilter.java

--- src/main/java/kr/mdns/madness/config/CorsConfig.java
+++ src/main/java/kr/mdns/madness/config/CorsConfig.java
@@ -10,22 +10,14 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    private final CorsProperties properties;
+    // private final CorsProperties properties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        var registration = registry.addMapping("/**")
-                .allowedMethods(properties.getAllowedMethods().toArray(String[]::new))
-                .allowedHeaders(properties.getAllowedHeaders().toArray(String[]::new))
-                .allowCredentials(properties.isAllowCredentials())
-                .maxAge(properties.getMaxAge());
-
-        if (!properties.getAllowedOriginPatterns().isEmpty()) {
-            registration.allowedOriginPatterns(
-                    properties.getAllowedOriginPatterns().toArray(String[]::new));
-        } else {
-            registration.allowedOrigins(
-                    properties.getAllowedOrigins().toArray(String[]::new));
-        }
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(1800);
     }
 }
