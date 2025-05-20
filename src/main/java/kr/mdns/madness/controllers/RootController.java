4e1eb703e17c861195e8e4294bf4894fc0e3523a
nera1 <nera4936@gmail.com>
2025-05-21 01:03:48 +0900

    feat: add healthcheck endpoint

--- src/main/java/kr/mdns/madness/controllers/RootController.java
+++ src/main/java/kr/mdns/madness/controllers/RootController.java
@@ -1,18 +1,13 @@
 package kr.mdns.madness.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
-    @GetMapping("/")
-    public ResponseEntity<Map<String, String>> root() {
-        Map<String, String> response = new HashMap<>();
-        response.put("msg", "Madness API");
-        return ResponseEntity.ok(response);
+    @GetMapping("/healthz")
+    public ResponseEntity<String> healthz() {
+        return ResponseEntity.ok("ok");
     }
 }
