bc01502c26f9c56d12cc0f6b3e072681bf25ff1c
nera1 <nera4936@gmail.com>
2025-06-19 18:47:55 +0900

    WIP: making websocket endpoint

---
+++ src/main/java/kr/mdns/madness/config/WebSocketConfigDev.java
@@ -0,0 +1,27 @@
+package kr.mdns.madness.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Profile("h2")
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfigDev implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry
+                .addEndpoint("/ws")
+                .setAllowedOriginPatterns("http://localhost:3000")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+}
