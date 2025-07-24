024e8f97d2af7e80ccc9947ffede9f073878f728
nera1 <nera4936@gmail.com>
2025-07-25 00:46:37 +0900

    fix: remove unused method

--- src/main/java/kr/mdns/madness/config/WebSocketConfig.java
+++ src/main/java/kr/mdns/madness/config/WebSocketConfig.java
@@ -45,9 +45,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         config.enableSimpleBroker("/sub");
         config.setApplicationDestinationPrefixes("/pub");
     }
-
-    @Override
-    public void configureClientOutboundChannel(ChannelRegistration registration) {
-        registration.interceptors(jwtAuthInterceptor);
-    }
 }
