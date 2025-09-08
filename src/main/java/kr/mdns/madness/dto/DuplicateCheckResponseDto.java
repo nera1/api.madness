163cc7402d16cbba15203a87fe4a9a4bc8a55bb5
nera1 <nera4936@gmail.com>
2025-09-08 23:54:22 +0900

    remove: remove unused dependencies

--- src/main/java/kr/mdns/madness/dto/DuplicateCheckResponseDto.java
+++ src/main/java/kr/mdns/madness/dto/DuplicateCheckResponseDto.java
@@ -14,10 +14,6 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class DuplicateCheckResponseDto {
-    /*
-     * field에 is가 들어가면 값이 제대로 Mapping되지 않을 가능성이 있어
-     * JSON key 값만 is가 붙도록 조정
-     */
     @JsonProperty("isDuplicate")
     private boolean duplicate;
 }
