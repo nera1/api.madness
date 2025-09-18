5bfa7940ddc2774096909a70edafc89c03a729c0
nera1 <nera4936@gmail.com>
2025-09-19 00:01:47 +0900

    fix: update ChannelConnectionCountScheduler

--- src/main/java/kr/mdns/madness/scheduler/ChannelConnectionCountScheduler.java
+++ src/main/java/kr/mdns/madness/scheduler/ChannelConnectionCountScheduler.java
@@ -3,12 +3,13 @@ package kr.mdns.madness.scheduler;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import kr.mdns.madness.services.ChannelConnectionCountService;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class ChannelConnectionCountScheduler {
-    private final ChannelConnectionCountScheduler channelConnectionCountScheduler;
+    private final ChannelConnectionCountService channelConnectionCountService;
 
     @Scheduled(cron = "0 0 * * * *", zone = "Asia/Seoul")
     public void sumChannelConnectionCountByCache() {
