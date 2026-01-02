2b26deabd502f40716b21c7dc34e05aa5033a73e
nera1 <nera4936@gmail.com>
2026-01-02 23:40:34 +0900

    chore: add securityFilterChain for deployment

--- src/main/kotlin/api/madn/es/config/SecurityConfigurationProduction.kt
+++ src/main/kotlin/api/madn/es/config/SecurityConfigurationProduction.kt
@@ -1,11 +1,22 @@
 package api.madn.es.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @Profile("production")
 class SecurityConfigurationProduction(
 ) {
-
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
+            .authorizeHttpRequests { it.anyRequest().permitAll() }
+        return http.build()
+    }
 }
