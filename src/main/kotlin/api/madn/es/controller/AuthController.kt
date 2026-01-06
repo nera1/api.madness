fcf4bfa008685c42767c7cf8de23b7682558f3e8
nera1 <nera4936@gmail.com>
2026-01-07 00:21:09 +0900

    feat: add signin, signup request

--- src/main/kotlin/api/madn/es/controller/AuthController.kt
+++ src/main/kotlin/api/madn/es/controller/AuthController.kt
@@ -1,7 +1,11 @@
 package api.madn.es.controller
 
+import api.madn.es.common.request.SignInRequest
+import api.madn.es.common.response.ApiResponse
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,17 +13,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/auth")
 class AuthController {
     @PostMapping("/signin")
-    fun login(): String {
-        return "login"
+    fun login(@Valid @RequestBody request : SignInRequest): ApiResponse<*> {
+        return ApiResponse.success(request)
     }
 
-    @PostMapping("/register")
-    fun register(): String {
-        return "register"
+    @PostMapping("/signup")
+    fun register(@Valid @RequestBody request : SignInRequest): ApiResponse<*> {
+        return ApiResponse.success(request)
     }
 
     @GetMapping("/signout")
-    fun logout(): String {
-        return "logout"
+    fun logout(): ApiResponse<*> {
+        return ApiResponse.success("signout")
     }
 }
